4f1f8d2c2ed92d7fcf17108b23b19f73636cb966
PDemenkov <guapnik@gmail.com>
2022-09-07 22:57:03 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int age = 8;
+        System.out.println("Значение переменной age с типом int = " + age );
+        byte speed = 127;
+        System.out.println("Значение переменной speed с типом byte = " + speed );
+        short maxLength = 300;
+        System.out.println("Значение переменной maxLength с типом short = " + maxLength );
+        long equator = 123L;
+        System.out.println("Значение переменной equator с типом long = " + equator );
+        float minWeight = 99F;
+        System.out.println("Значение переменной minWeight с типом float = " + minWeight );
+        double maxWeight = 3.14;
+        System.out.println("Значение переменной pi с типом double = " + maxWeight );
+    }
+}
