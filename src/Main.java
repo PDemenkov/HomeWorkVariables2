6c0d08dd2335abb48136854c270ee22c3edbfd67
PDemenkov <guapnik@gmail.com>
2022-09-08 17:46:14 +0300

    Задачи 2-8 fix

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         System.out.println("Задача 2");
         float f =27.12F;
-        long l =987678965549L;
+        long l =987_678_965_549L;
         double d =2.786;
         boolean b=false;
         short s =569;
@@ -32,11 +32,21 @@ public class Main {
         System.out.println("На каждого ученика рассчитано " +totalSheets/(lP+aS+eA) + " листов бумаги");
 
         System.out.println("Задача 4");
-        int bottle = 8;
-        System.out.println("За 20 минут работы машины произвела бутылок " + bottle*20 + (" штук"));
-        System.out.println("За 24 часа машины произвела бутылок " + bottle*1440 + (" штук"));
-        System.out.println("За 3 дня машины произвела бутылок " + bottle*1440*3 + (" штук"));
-        System.out.println("За 1 месяц машины произвела бутылок " +bottle*31*1440 + (" штук"));
+//      int bottle = 8;
+        int bottle = 16;
+        int bottleInMinute = bottle / 2;
+        int twentyMinutes = 20;
+        System.out.println("За " + twentyMinutes +" минут работы машины произвела бутылок " + bottleInMinute*twentyMinutes + (" штук"));
+        int day=60*24;
+        System.out.println("За " + day +" минут работы машины произвела бутылок " + bottleInMinute*day + (" штук"));
+        int threeDays=day*3;
+        System.out.println("За " + threeDays +" минут работы машины произвела бутылок " + bottleInMinute*threeDays + (" штук"));
+        int month=day*31;
+        System.out.println("За " + month +" минут работы машины произвела бутылок " + bottleInMinute*month + (" штук"));
+//        System.out.println("За 20 минут работы машины произвела бутылок " + bottle*20 + (" штук"));
+//        System.out.println("За 24 часа машины произвела бутылок " + bottle*1440 + (" штук"));
+//        System.out.println("За 3 дня машины произвела бутылок " + bottle*1440*3 + (" штук"));
+//        System.out.println("За 1 месяц машины произвела бутылок " +bottle*31*1440 + (" штук"));
 
         System.out.println("Задача 5");
         int tinTotal = 120;
